5dcdf098b95e7ea90323d6e4dac62687982c3471
Ivan <mascomen4@gmail.com>
2021-09-03 22:50:33 +0300

    finishing 1 lab 1 ex

--- lab01/src/calc_array_bounds.f90
+++ lab01/src/calc_array_bounds.f90
@@ -10,7 +10,7 @@ contains
 !   max_threads_num - общее число потоков
 !   bounds - начальный и конечный индексы массива
 !   slice - результат вычислений
- pure function calc_vector_slice(thread_num, max_threads_num, bounds) result (slice)
+ function calc_vector_slice(thread_num, max_threads_num, bounds) result (slice)
   implicit none
   integer(int32), intent(in) :: thread_num, max_threads_num
   integer(int64), intent(in), dimension(1:2) :: bounds
@@ -22,15 +22,19 @@ contains
   real :: difference
 
   difference = (bounds(2) - bounds(1)) / max_threads_num
-  ! write (*, *), "Difference ", difference
+  write (*, *), "Difference ", difference
   slice_size = floor( difference )
+  write (*, *), "Slice size", slice_size
 
   if (thread_num == max_threads_num - 1) then
-    slice_left = bounds(1) + thread_num*(slice_size - 1)
-    slice_right = slice_left + slice_size - 1
+      slice_left = bounds(1) + thread_num*(slice_size)
+      slice_right = bounds(2)
+  else if (thread_num == 0) then
+      slice_left = bounds(1)
+      slice_right = slice_left + slice_size - 1
   else
-    slice_left = bounds(1) + thread_num*(slice_size - 1)
-    slice_right = max_threads_num
+      slice_left = bounds(1) + thread_num*(slice_size)
+      slice_right = slice_left + slice_size - 1
   end if
 
   slice = [slice_left, slice_right]
@@ -44,12 +48,15 @@ end function calc_vector_slice
 !   max_threads_num - общее число потоков
 !   bounds - начальный и конечный индексы массива по двум измерениям
 !   slice - границы вырезки массива
-pure function calc_matrix_slice(th_i, th_j, max_threads_num, bounds) result(slice)
+function calc_matrix_slice(th_i, th_j, max_threads_num, bounds) result(slice)
   implicit none
   integer(int32), intent(in) :: th_i, th_j, max_threads_num
   integer(int64), dimension(1:2,1:2), intent(in) :: bounds
   integer(int64), dimension(1:2,1:2) :: slice
   ! Здесь должен быть ваш код
+
+    slice(1, :) = calc_vector_slice(th_i, max_threads_num, bounds(1, :))
+    slice(2, :) = calc_vector_slice(th_j, max_threads_num, bounds(2, :))
 end function calc_matrix_slice
 
 end module calc_array_bounds
