63d528fb03615103f623558bb323bd5eb1e93db5
Ivan <mascomen4@gmail.com>
2021-09-04 09:18:38 +0300

    ex 1,2 completed

--- cmake-build-debug/CMakeFiles/parallel_programming.dir/cmake_clean_Fortran.cmake
+++ cmake-build-debug/CMakeFiles/parallel_programming.dir/cmake_clean_Fortran.cmake
@@ -1,6 +1,6 @@
 # Remove fortran modules provided by this target.
 FILE(REMOVE
-  "calc_array_bounds.mod"
-  "CALC_ARRAY_BOUNDS.mod"
-  "CMakeFiles/parallel_programming.dir/calc_array_bounds.mod.stamp"
+  "quick_sort.mod"
+  "QUICK_SORT.mod"
+  "CMakeFiles/parallel_programming.dir/quick_sort.mod.stamp"
   )
